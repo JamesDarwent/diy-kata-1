884deca25bea992d25d77f95d06bc803674f9bf4
James Darwent <jamesdarwent_88@hotmail.co.uk>
2021-01-06 16:05:41 +0000

    reachDestination test completed

--- src/kata5.reachDestination.js
+++ src/kata5.reachDestination.js
@@ -1,3 +1,10 @@
-const reachDestination = (distance, speed) => {};
+const reachDestination = (distance, speed) => {
+    
+    const time = (distance / speed);
+    const eta = Math.round(time / 0.5) * 0.5;
+
+    return `I should be there in ${eta} hours`;
+};
+
 
 module.exports = reachDestination;
--- test/kata5.reachDestination.test.js
+++ test/kata5.reachDestination.test.js
@@ -1,5 +1,17 @@
 const { reachDestination } = require("../src");
 
 describe("reachDestination", () => {
-  test("returns string with estimated time of arrival", () => {});
+  test("returns string with estimated time of arrival", () => {
+    expect(reachDestination(44, 10)).toEqual("I should be there in 4.5 hours");
+    expect(reachDestination(80, 33)).toEqual("I should be there in 2.5 hours");
+    expect(reachDestination(277, 53)).toEqual("I should be there in 5 hours");
+    expect(reachDestination(542, 97)).toEqual("I should be there in 5.5 hours");
+  });
 });
+
+
+// Formula for estimated time of arrival
+
+// Distance / speed, round up to the nearest 0.5
+
+
