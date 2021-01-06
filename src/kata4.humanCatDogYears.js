362f440c48015492653b21ac9abe3f3fd9d78106
James Darwent <jamesdarwent_88@hotmail.co.uk>
2021-01-06 02:33:12 +0000

    humanCatDogYears test completed

--- src/kata4.humanCatDogYears.js
+++ src/kata4.humanCatDogYears.js
@@ -1,3 +1,9 @@
-const humanCatDogYears = number => {};
+const humanCatDogYears = number => {
+    
+    const humanToCat = (number - 2) * 4 + 24;
+    const humanToDog = (number - 2) * 5 + 24;
+
+    return [number, humanToCat, humanToDog];
+};
 
 module.exports = humanCatDogYears;
--- test/kata4.humanCatDogYears.test.js
+++ test/kata4.humanCatDogYears.test.js
@@ -1,3 +1,23 @@
+const { TestScheduler } = require("jest");
 const { humanCatDogYears } = require("../src");
 
-// Look Ma, no handlebars!!!
+describe("humanCatDogYears", () => {
+    test("returns a revised array of containing human, cat and dog years", () => {
+        expect(humanCatDogYears(10)).toEqual([10, 56, 64]);
+        expect(humanCatDogYears(20)).toEqual([20, 96, 114]);
+        expect(humanCatDogYears(46)).toEqual([46, 200, 244]);
+    });
+});
+
+
+// Formulas for calculating cat and dog years 
+
+// Cat Years
+// 15 cat years for first human year
+// +9 cat years for second human year
+// +4 cat years for each human year after that
+
+// Dog Years
+// 15 dog years for first year
+// +9 dog years for second year
+// +5 dog years for each human year after that
