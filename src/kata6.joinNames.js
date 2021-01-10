ea9a05697551a69fa2cae58e4e9c7e51966e2e74
James Darwent <jamesdarwent_88@hotmail.co.uk>
2021-01-10 21:06:43 +0000

    joinNames test completed

--- src/kata6.joinNames.js
+++ src/kata6.joinNames.js
@@ -1,3 +1,22 @@
-const joinNames = namesObj => {};
+const joinNames = namesObj => {
+
+    let allNames;
+
+    const arrayOfNames = namesObj.map(element => element.name);
+
+    if (arrayOfNames.length === 1) {
+        allNames = arrayOfNames[0];
+    }
+
+    else if (arrayOfNames.length === 2) {
+        allNames = `${arrayOfNames[0]} & ${arrayOfNames[1]}`;
+    }
+    
+    else if (arrayOfNames.length > 2) {
+        allNames = `${arrayOfNames.slice(0, -1).join(', ')} & ${arrayOfNames.slice(-1)}`;
+    }
+
+    return allNames;
+};
 
 module.exports = joinNames;
--- test/kata6.joinNames.test.js
+++ test/kata6.joinNames.test.js
@@ -1,5 +1,41 @@
 const { joinNames } = require("../src");
 
 describe("joinNames", () => {
-  test("returns string of names, seperated by commas and an ampersand", () => {});
+  test("returns string of names, seperated by commas and an ampersand", () => {
+    expect(joinNames([{
+      name: 'Bart'
+    }, {
+      name: 'Lisa'
+    }, {
+      name: 'Maggie'
+  }])).toEqual("Bart, Lisa & Maggie");
+ 
+    expect(joinNames([{
+      name: 'Tom'
+    }, {
+      name: 'Jerry'
+  }])).toEqual("Tom & Jerry");
+
+    expect(joinNames([{
+      name: 'Luffy'
+    }, {
+      name: 'Zoro'
+    }, {
+      name: 'Nami'
+    }, {
+      name: 'Ussop'
+    }, {
+      name: 'Sanji'
+    }, { 
+      name: 'Chopper'
+    }, {
+      name: 'Robin'
+    }, {
+      name: 'Franky'
+    }, {
+      name: 'Brook'
+    }, {
+      name: 'Jinbe'
+  }])).toEqual("Luffy, Zoro, Nami, Ussop, Sanji, Chopper, Robin, Franky, Brook & Jinbe");
+  });
 });
