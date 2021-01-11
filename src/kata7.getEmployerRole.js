bf71bd8857c85f7b5fe7f22de6aa314e490c3c50
James Darwent <jamesdarwent_88@hotmail.co.uk>
2021-01-11 17:35:03 +0000

    getEmployerRole test completed

--- src/kata7.getEmployerRole.js
+++ src/kata7.getEmployerRole.js
@@ -1,3 +1,13 @@
-const getEmployerRole = (employeeName, employees) => {};
+const getEmployerRole = (employeeName, employees) => {
+
+    let roles;
+
+    employees.map(element => {
+        if(employeeName === element.name){
+        roles = element.role 
+    }});
+        
+    return roles;
+};
 
 module.exports = getEmployerRole;
